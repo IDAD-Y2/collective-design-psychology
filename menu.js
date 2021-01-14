1a3d26700be2b41eb0904dff9242bb821a0226e4
elouanlerouxel <72791170+elouanlerouxel@users.noreply.github.com>
2021-01-14 11:17:05 +0000

    New sidenav

--- menu.js
+++ menu.js
@@ -5,14 +5,15 @@ document.write( '</span>\n' );
 document.write( '</span>\n' );
 document.write( '</button>\n' );
 document.write( '<div id=\"nav\" class=\"sidenav\">\n' );
-document.write( '<a href=\"index.html\"> Home </a>\n' );
-document.write( '<a href=\"dark-patterns.html\"> Dark Patterns </a>\n' );
-document.write( '<a href=\"gamification.html\"> Gamification </a>\n' );
-document.write( '<a href=\"cognitive-bias.html\"> Cognitive Bias in Design </a>\n' );
-document.write( '<a href=\"rights-and-wrongs-online.html\"> Rights and wrongs of online activity </a>\n' );
-document.write( '<a href=\"digital-privacy.html\"> Digital privacy </a>\n' );
-document.write( '<a href=\"solutionism.html\"> Solutionism </a>\n' );
-document.write( '<a href=\"digital-accessibility.html\"> Digital Accessibility </a>\n' );
+document.write( '<a href=\"index.html\"> Home <br><span>This is the home page</span><span class=\"arrow\">→</span></a>\n' );
+document.write( '<a href=\"dark-patterns.html\"> Dark Patterns <br><span>The dark side of the web</span><span class=\"arrow\">→</span></a>\n' );
+document.write( '<a href=\"gamification.html\"> Gamification <br><span>The use of gaming elements in UI Design</span><span class=\"arrow\">→</span></a>\n' );
+document.write( '<a href=\"cognitive-bias.html\"> Cognitive Bias in Design <br><span>A shortcut taken by our mind</span><span class=\"arrow\">→</span></a>\n' );
+document.write( '<a href=\"rights-and-wrongs-online.html\"> Rights and wrongs of online activity <br><span>We are all capable of making decisions , what will you choose ?</span><span class=\"arrow\">→</span></a>\n' );
+document.write( '<a href=\"digital-privacy.html\"> Digital privacy <br><span>How private your information is on the internet</span><span class=\"arrow\">→</span></a>\n' );
+document.write( '<a href=\"solutionism.html\"> Solutionism <br><span>Solutionism is the idea that any problem can be solved through technology.</span><span class=\"arrow\">→</span></a>\n' );
+document.write( '<a href=\"digital-accessibility.html\"> Digital Accessibility <br><span>Making computers and the web usable for all</span><span class=\"arrow\">→</span></a>\n' );
+document.write('<a target=\"_blank\" href=\"https://github.com/IDAD-Y2/collective-design-psychology\" id=\"bottom\">Made collectively by Year 2 IDAD Students<br><span>Link to the GITHUB repository</span></a>')
 document.write( '</div>' );
 
 function openNav() {
